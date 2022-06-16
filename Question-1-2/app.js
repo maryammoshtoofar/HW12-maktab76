1a0b828e3aba9880b3ed3d5e8dde59114e64a0b5
Maryam Moshtoofar <maryammoshtoofar@gmail.com>
2022-06-17 02:38:13 +0430

    added q1 and 2

---
+++ Question-1-2/app.js
@@ -0,0 +1,30 @@
+// // q2 part 1
+
+const fname = document.getElementById("fname");
+fname.addEventListener("focusout", () => {
+  const isEmpty = !fname.value;
+  const validText = document.getElementsByClassName("validText")[0];
+  const invalidText = document.getElementsByClassName("invalidText")[0];
+  if (isEmpty) {
+    fname.classList.add("invalid");
+    invalidText.classList.remove("hide");
+    validText.classList.add("hide");
+  } else {
+    fname.classList.remove("invalid");
+    fname.classList.add("valid");
+    validText.classList.remove("hide");
+    invalidText.classList.add("hide");
+  }
+});
+
+// q2 part 2
+const email = document.getElementById("email");
+const leftEmail = document.getElementById("leftEmail");
+email.addEventListener("change", () => {
+  leftEmail.innerHTML = email.value;
+});
+
+// q2 part 3
+document.getElementById("contactForm").addEventListener("submit", (event) => {
+  event.preventDefault();
+});
