a70596aa39f6e225cbdac50a990b512100679231
Maryam Moshtoofar <maryammoshtoofar@gmail.com>
2022-06-28 20:11:19 +0430

    added dispatch

--- Question-6/app.js
+++ Question-6/app.js
@@ -1,9 +1,12 @@
 const elem = document.getElementById("elemId");
-let goodBye = new Event("click", { bubbles: true });
-elem.dispatchEvent(goodBye);
-
-document.addEventListener("goodBye", function (event) {
-  console.log("hi");
-  console.log(`clientX =${event.clientX}`);
-  console.log(`clientY =${event.clientY}`);
+// catch on document...
+document.addEventListener("hello", function (event) {
+  // (1)
+  alert("Hello from " + event.target.tagName); // Hello from H1
 });
+
+// ...dispatch on elem!
+let event = new Event("hello", { bubbles: true }); // (2)
+elem.dispatchEvent(event);
+
+// the handler on document will activate and display the message.
