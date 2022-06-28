b543b0160f524fb7bb70c4c0eb0e04582f754bdc
Maryam Moshtoofar <maryammoshtoofar@gmail.com>
2022-06-28 20:22:54 +0430

    changed question 8

--- Question-6/app.js
+++ Question-6/app.js
@@ -1,12 +1,4 @@
 const elem = document.getElementById("elemId");
-// catch on document...
-document.addEventListener("hello", function (event) {
-  // (1)
-  alert("Hello from " + event.target.tagName); // Hello from H1
-});
-
 // ...dispatch on elem!
-let event = new Event("hello", { bubbles: true }); // (2)
+let event = new Event("goodbye", { bubbles: true }); // (2)
 elem.dispatchEvent(event);
-
-// the handler on document will activate and display the message.
--- Question-8/app.js
+++ Question-8/app.js
@@ -5,5 +5,9 @@ lodashCDN.setAttribute(
   "https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.js"
 );
 document.body.append(lodashCDN);
-CDNVersion.innerHTML = `Lodash Version is = ${lodashCDN.src.split("/")[6]}`;
 document.body.append(CDNVersion);
+
+lodashCDN.addEventListener("load", function () {
+  // CDNVersion.innerHTML = `Lodash Version is = ${lodashCDN.src.split("/")[6]}`;
+  CDNVersion.innerHTML = `Lodash Version is = ${_.VERSION}`;
+});
